4bcce3ac36f5750613f6c3c8c2a1758839532832
Yunspace <yun@yunspace.com>
2018-04-08 22:50:58 +1000

    updated custom function

--- go-faster/main.go
+++ go-faster/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/yunspace/aws-lambda-go-faster/lambda"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func Handler(request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	return &events.APIGatewayProxyResponse{Body: "hello", StatusCode: 200}, nil
+// custom handler that doesn't rely on reflection
+type handler func() (*events.APIGatewayProxyResponse, error)
+
+// Invoke is required by the interface to pass through byte array received from RPC call
+func (h handler) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
+	resp, err := h()
+	if err != nil {
+		return nil, err
+	}
+
+	respBytes, err := json.Marshal()
+	if err != nil {
+		return nil, err
+	}
+
+	return respBytes, err
 }
 
+var _ lambda.LambdaHander = (handler)(nil)
+
 func main() {
-	lambda.Start(Handler)
+	lambda.StartWrapper(Handler)
 }
