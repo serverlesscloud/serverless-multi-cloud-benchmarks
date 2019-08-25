d4a706aa6199f6afa7953cd971c611774bb90520
Yun Zhi Lin <yun.zhi.lin@contino.io>
2019-08-26 02:48:36 +1000

    updated java version

--- gcp/gcp-nodejs10/function.js
+++ gcp/gcp-nodejs10/function.js
@@ -3,17 +3,3 @@
 exports.hello = (request, response) => {
   response.status(200).send('hello');
 };
-
-exports.users = (request, response) => {
-  switch (request.method) {
-    case 'GET':
-      response.status(200).send('user');
-      break;
-    case 'PUT':
-      response.status(201).send('created');
-      break;
-    default:
-      response.status(500).send('unsupported method');
-      break;
-  }
-};
